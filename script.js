b2318dc78a41b7a0e7bc1c5e33cfadb87e47c02c
Fathimahenna <99035986+Fathimahenna@users.noreply.github.com>
2024-07-18 13:47:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 function downloadPDF() {
     // Replace 'YourFileName.pdf' with the actual file name of your PDF
-    var fileUrl = "Playbook.pdf"; // Assuming 'YourFileName.pdf' is in the same repository
+    var fileUrl = "QAQCManual.pdf"; // Assuming 'YourFileName.pdf' is in the same repository
     var xhr = new XMLHttpRequest();
     xhr.open("GET", fileUrl, true);
     xhr.responseType = "blob";
@@ -10,7 +10,7 @@ function downloadPDF() {
         var blob = xhr.response;
         var link = document.createElement('a');
         link.href = window.URL.createObjectURL(blob);
-        link.download = "Playbook.pdf"; // Adjust the file name if necessary
+        link.download = "QAQCManual.pdf"; // Adjust the file name if necessary
         link.click();
     };
     xhr.send();
